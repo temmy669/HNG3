b8d7da67f72efd1faf33788d8df811d48c09e190
temmy <favouradebose@gmail.com>
2025-10-25 18:48:02 +0100

    updated allowed host settings

--- HNG3/settings.py
+++ HNG3/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!_$lkirhx^@o_1@=d6h1*e-bodivlo_sl-yr7co!%l)kae&^*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
